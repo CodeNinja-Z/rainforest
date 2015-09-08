00b85d912113119283f29b470083108272b4aeb7
Heart Beat Studio <blacklight728@gmail.com>
2015-09-07 22:59:57 -0400

    Adding logic to display 'Signed in as' or 'Login or Sign up' in the application view. At this point, this app only allow ONE user to log in at one time.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,27 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  private
+
+  def current_user
+    # Q: What's the purpose of ||= ?
+    # A: ||是 或运算符···如果前面的变量为空··那么就会执行后面的表达式··进行赋值
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    # Q: Session is something that should be at top of a web service
+    # How do we access the session hash at here from 
+    # sessions_controller.rb when we don't include anything?
+  end
+
+  # Why define a private method and use it as helper method
+  # WHy not define a public method?
+  helper_method :current_user
 end
+
+
+# Q: Why define current_user in ApplicationController?
+# A: method defined in ApplicationController can be invoked
+#    by other user-defined Controllers
+#    But if a helper method is defined in a user-defined
+#    Controller, that method can't be accessed by other
+#    user-defined Controllers
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,11 +9,13 @@ class SessionsController < ApplicationController
       redirect_to products_url, notice: "Logged in!" # Q: Where can I see this notice message? Who this message is written for?
     else
       flash.now[:alert] = "Invalid email or password" # Q: It works the same with or withour '.now'
+      # alert: "Invalid email or password" # Can't write like this, causes error
       render "new"
     end
   end
 
   def destroy
+    # Up to now, this app only allow ONE user to log in at one time
     session[:user_id] = nil
     redirect_to products_url, notice: "Logged out!"
   end
