4b966f2e5c9b324538d10c46e757b340366aedb2
Heart Beat Studio <blacklight728@gmail.com>
2015-09-07 21:16:33 -0400

    Adding flash alerts and notices

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,9 +5,10 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:email])
     if user && user.authenticate(params[:password])
-      session[:user_id] = user.id
-      redirect_to products_url, notice: "Logged in!"
+      session[:user_id] = user.id # This is how to set a hash's key with a value
+      redirect_to products_url, notice: "Logged in!" # Q: Where can I see this notice message? Who this message is written for?
     else
+      flash.now[:alert] = "Invalid email or password" # Q: It works the same with or withour '.now'
       render "new"
     end
   end
